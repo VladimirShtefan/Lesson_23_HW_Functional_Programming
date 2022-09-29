df6d7d14577562d5f77d5e32e37cd1a07d70e352
VladmimirShtefan <shtefanvladimir14@gmail.com>
2022-09-29 11:53:44 +0700

    созданы методы для работы с переданными командами

--- application/app.py
+++ application/app.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
-from flask import Flask, request
+from flask import Flask, request, abort
 
+from application.service.commands import RunCommands
 from application.service.datafile import DataFile
 from application.service.request import RequestParser
 
@@ -18,5 +19,11 @@ def perform_query():
     file_name = Path.joinpath(DATA_PATH, parser.file_name)
     query = parser.query
     file = DataFile(file_name)
+    if not file.check_file():
+        abort(400, 'Не верный путь к файлу')
     user_request = []
+    try:
+        user_request = list(RunCommands(file.read(), **query).run())
+    except IndexError:
+        abort(400, f'При использовании команды map выбран не существующий столбец')
     return app.response_class('\n'.join(user_request), content_type="text/plain")
